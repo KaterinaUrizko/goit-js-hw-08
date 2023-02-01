f6ed982ae2a63617f8c2813ebbc1bdb773a81ea0
Katerina Urizko <k.urizko@gmail.com>
2023-02-01 15:46:19 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,7 +5,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(galleryItems);
+
 
 const imageContainer = document.querySelector('.gallery')
  
