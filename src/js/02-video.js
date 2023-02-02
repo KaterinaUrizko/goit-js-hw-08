d3767f9c49bbc1677349c222a8759fa7368c7fb4
Katerina Urizko <k.urizko@gmail.com>
2023-02-02 19:47:59 +0200

    deleted comments

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,13 +13,9 @@ function onPlaying(e) {
 
     const playingTime = e.seconds;
 
-    // const time = player.getCurrentTime();
 
     localStorage.setItem(STORAGE_KEY, playingTime);
 };
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
 const currentTime = localStorage.getItem(STORAGE_KEY);
 
 const startTime = JSON.parse(currentTime );
@@ -35,33 +31,8 @@ console.log(startTime);
      }
  });
 
-// import Player from '@vimeo/player';
-
-  
-// const iframe = document.querySelector('iframe');
-// const iframePlayer = new Vimeo.Player(iframe);
-
-// const CURRENT_TIME = "videoplayer-current-time";
 
 
-// player.setCurrentTime(JASON.parse(CURRENT_TIME)).then(function(seconds) {
-//    console.log("CurrentTime")
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
-// визначити поточний час 
-// записати ці дані в localStorage
-// повернути ці дані з LocalSTorage при запуску player SetCurrentTime 
-// (JSON.parse(localStorage.setItem(time.Now.seconds)))
 
 
 
