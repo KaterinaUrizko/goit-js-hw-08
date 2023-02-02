a05c1a3cf209f3f96f436ad76e9ffea9743fb8fa
Katerina Urizko <k.urizko@gmail.com>
2023-02-02 19:42:24 +0200

    2 tasks added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,69 @@
 
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.getElementById("vimeo-player");
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlaying, 1000));
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function onPlaying(e) {
+
+    const playingTime = e.seconds;
+
+    // const time = player.getCurrentTime();
+
+    localStorage.setItem(STORAGE_KEY, playingTime);
+};
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+const startTime = JSON.parse(currentTime );
+console.log(startTime);
+
+ player.setCurrentTime(currentTime).then(function(seconds) {
+ }).catch(function(error) {
+     switch (error.name) {
+       case 'RangeError':
+            break;
+        default:
+            break;
+     }
+ });
+
+// import Player from '@vimeo/player';
+
+  
+// const iframe = document.querySelector('iframe');
+// const iframePlayer = new Vimeo.Player(iframe);
+
+// const CURRENT_TIME = "videoplayer-current-time";
+
+
+// player.setCurrentTime(JASON.parse(CURRENT_TIME)).then(function(seconds) {
+//    console.log("CurrentTime")
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+
+// визначити поточний час 
+// записати ці дані в localStorage
+// повернути ці дані з LocalSTorage при запуску player SetCurrentTime 
+// (JSON.parse(localStorage.setItem(time.Now.seconds)))
+
+
+
+    
+    
