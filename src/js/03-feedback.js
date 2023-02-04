28b59cd2594a442d2fda5491240dc18fb905e826
Katerina Urizko <k.urizko@gmail.com>
2023-02-04 17:15:03 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,50 +13,45 @@ const refs = {
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
+const formData = {};
+const storagedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
 
-const formData ={};
 
+  refs.form.elements.email.value = storagedData?.email || '';
+  refs.form.elements.message.value = storagedData?.message || ''; 
 
-populateTextarea();
+// populateTextarea();
 
 function onFormInput(event) {
   
   formData[event.target.name] = event.target.value;
-  
-  const storagedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
 
-   localStorage.setItem( STORAGE_KEY,
+  localStorage.setItem( STORAGE_KEY,
     JSON.stringify({ ...storagedData, [event.target.name] : event.target.value  })
   );
-
-
 }
 
 
 function onFormSubmit(event) {
 
     event.preventDefault();
-
-   
+  
     event.currentTarget.reset();
  
     localStorage.removeItem(STORAGE_KEY);
   
  }
 
-function populateTextarea() {
-
-    
-  try {
-  const storagedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-refs.form.elements.email.value = storagedData?.email || '';
- refs.form.elements.message.value = storagedData?.message || '';
-    
+// function populateTextarea() {
   
-  }
-   catch (error) {
-      console.log("parsing error");
-    }
-}
+  // try {
+
+    // refs.form.elements.email.value = storagedData?.email || '';
+    // refs.form.elements.message.value = storagedData?.message || ''; 
+  
+  // }
+  //  catch (error) {
+  //     console.log("parsing error");
+  //   }
+// }
   
